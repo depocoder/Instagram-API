a615243a547ac301b1ae1c9af6b587f7ae6b284a
herypank <60302361+herypank@users.noreply.github.com>
2020-08-09 15:34:57 +0300

    Update inst_parser.py

--- inst_parser.py
+++ inst_parser.py
@@ -3,10 +3,16 @@ from pathlib import Path
 import requests
 
 
-url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-response = requests.get(url)
-Path(os.getcwd(), 'images').mkdir(parents=True, exist_ok=True)
-folder = os.path.join(os.getcwd(), 'images', 'filename.jpeg')
-with open(folder, 'wb') as file:
-    file.write(response.content)
+def download_img():
+    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    response = requests.get(url)
+    folder = os.path.join(os.getcwd(), 'images', 'filename.jpeg')
+    with open(folder, 'wb') as file:
+        return file.write(response.content)
+
+
+if __name__ == "__main__":
+    Path(os.getcwd(), 'images').mkdir(parents=True, exist_ok=True)
+    download_img()
+
 
