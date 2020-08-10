5ffa6e5d61dad467a1cb8c14d15abd9f6427752d
herypank <60302361+herypank@users.noreply.github.com>
2020-08-10 12:53:25 +0300

    Update inst_parser.py

--- inst_parser.py
+++ inst_parser.py
@@ -9,7 +9,7 @@ def parse_links():
     return response.json()['links']["flickr"]['original']
 
 
-def download_img(link, num_pic):
+def download_img(num_pic, link):
     response = requests.get(link)
     folder = os.path.join(os.getcwd(), 'images', f"spacex{num_pic}.jpg")
     with open(folder, 'wb') as file:
@@ -20,4 +20,4 @@ if __name__ == "__main__":
     base_url = 'https://api.spacexdata.com/v3'
     Path(os.getcwd(), 'images').mkdir(parents=True, exist_ok=True)
     for num_pic, link in enumerate(parse_links(), 1):
-        download_img(link, num_pic)
+        download_img(num_pic, link)
