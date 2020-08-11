0ce2e5b9ae03935d886470c31ae9e28eeceba7d3
herypank <60302361+herypank@users.noreply.github.com>
2020-08-11 23:29:26 +0300

    Update inst_parser.py

--- inst_parser.py
+++ inst_parser.py
@@ -1,15 +1,15 @@
 import os
 from pathlib import Path
 import argparse
-import time
 from io import open
 from urllib.parse import urljoin
 import requests
 from PIL import Image
 from instabot import Bot
 
+
 def parse_ids():
-    url = 'http://hubblesite.org/api/v3/images/wallpaper'
+    url = 'http://hubblesite.org/api/v3/images/spacecraft'
     response = requests.get(url)
     response.raise_for_status()
     ids = [id['id'] for id in response.json()]
@@ -46,6 +46,34 @@ def download_content(link, id):
         return file.write(response.content)
 
 
+def calculate_the_size(width, height):
+    if (width / height) < (4 / 3):
+        width = (width // 4) * 4
+        height = (width // 4) * 3
+    else:
+        height = (height // 3) * 3
+        width = (height // 3) * 4
+    return width, height
+
+
+def upload_photo(id, filename_extension):
+    image = Image.open(f"images/{id}.{filename_extension}")
+    image.thumbnail((1080, 1080))
+    if image.format == 'PNG':
+        image = image.convert("RGB")
+        path = os.path.join(os.getcwd(), 'images', f'{id}.png')
+        os.remove(path)
+    crop_width, crop_height = calculate_the_size(
+        image.width, image.height)
+    coordinates = (
+        image.width - crop_width, image.height - crop_height,
+        image.width, image.height)
+    image = image.crop(coordinates)
+    image.save(f"images/{id}.jpg")
+    return bot.upload_photo(
+        os.path.join(os.getcwd(), 'images', f'{id}.jpg'), caption="Nice pic!")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--username', nargs='?',
@@ -63,13 +91,6 @@ if __name__ == "__main__":
         link = choice_better_img(links)
         filename_extension = link.split('.')[-1]
         download_content(link, id)
-        image = Image.open(f"images/{id}.{filename_extension}")
-        image.thumbnail((1080, 1080))
-        if image.format == 'PNG':
-            image = image.convert("RGB")
-            path = os.path.join(os.getcwd(), 'images', f'{id}.png')
-            os.remove(path)
-        image.save(f"images/{id}.jpg")
-        bot.upload_photo(
-        os.path.join(os.getcwd(), 'images', '3850.jpg'), caption="Nice pic!")
-    
+        upload_photo(id, filename_extension)
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'images')
+    os.rmdir(path)
