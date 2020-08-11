eb9d91683c40021cac226fc06dab51e4b43bbc32
herypank <60302361+herypank@users.noreply.github.com>
2020-08-11 16:07:30 +0300

    Добавлена функция choice_better_img

--- inst_parser.py
+++ inst_parser.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from random import choice
 from urllib.parse import urljoin
 import requests
 
@@ -18,10 +17,21 @@ def parse_link_hubble(id):
     response = requests.get(url)
     response.raise_for_status()
     links = response.json()["image_files"]
-    link = choice(links)['file_url'].split(
+    return links
+
+
+def choice_better_img(links):
+    max = 0
+    for num_link, link in enumerate(links):
+        filename_extension = link['file_url'].split('.')[-1]
+        if link['file_size'] > max and (
+                filename_extension == 'jpg' or filename_extension == 'png'):
+            max = link['file_size']
+            max_num = num_link
+    link = links[max_num]['file_url'].split(
         'imgsrc.hubblesite.org/hvi/uploads/')
     return urljoin('https://media.stsci.edu/uploads/', link[1])
-     
+
 
 def download_content(link, id):
     response = requests.get(link)
@@ -34,9 +44,8 @@ def download_content(link, id):
 
 if __name__ == "__main__":
     Path(os.getcwd(), 'images').mkdir(parents=True, exist_ok=True)
-    #for num_pic, link in enumerate(parse_links(), 1):
-        #fetch_spacex_last_launch(num_pic, link)
     ids = parse_ids()
     for id in ids:
-        link = parse_link_hubble(id)
+        links = parse_link_hubble(id)
+        link = choice_better_img(links)
         download_content(link, id)
